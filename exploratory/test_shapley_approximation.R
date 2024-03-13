648be07e933b4423b2f7277091d77908a0e110e6
Nathaniel Henry <nat.henry7@gmail.com>
2024-03-12 22:05:06 -0700

    Test timing of Shapley approximation for many possible values.

--- exploratory/test_shapley_approximation.R
+++ exploratory/test_shapley_approximation.R
@@ -21,51 +21,81 @@ CONFIG_PATH <- '~/repos/usaid-mch-ml/config.yaml'
 ## SETUP -------------------------------------------------------------------------------->
 
 # Load packages
-load_libs <- c('versioning', 'data.table', 'caret')
+load_libs <- c('versioning', 'data.table', 'caret', 'ggplot2')
+lapply(load_libs, library, character.only = T) |> invisible()
 
 # Load helper R package
 config <- versioning::Config$new(CONFIG_PATH)
 devtools::load_all(config$get_dir_path('r_package'))
 
+# Maximum dimensions to test: 35k observations by 100 variables ------------------------->
 
-## CREATE TEST DATASET ------------------------------------------------------------------>
+n_obs <- 3.5e4
+features_space <- c(seq(5, 25, by = 5), seq(30, 100, by = 10))
+interaction_terms <- FALSE
 
-# Simple linear model
-n_observations <- 1e3
-model_data <- data.table::data.table(
-  a = rnorm(n_observations), b = rnorm(n_observations), c = rnorm(n_observations),
-  d = rnorm(n_observations), e = rnorm(n_observations), f = rnorm(n_observations),
-  error = rnorm(n_observations)
-)[, outcome := .5  * a + .2 * b + .1 * c  - .1 * d - .2 * e - .5 * f + .5 * error ]
+for(n_features in features_space){
+  message("Running with ", n_features, " features")
+  tictoc::tic(n_features)
+  # Simulate the dataset
+  model_data <- data.table(error = rnorm(n_obs))
+  model_data[, outcome := error ]
 
-loss_fun <- mch.ml::get_loss_function('L2')
+  for(ii in seq_len(n_features)){
+    vv <- paste0('v', ii)
+    model_data[[vv]] <- rnorm(n_obs)
+    model_data$outcome <- model_data$outcome + rnorm(1) * model_data[[vv]]
+  }
 
-# Fit the model
-model_fit <- caret::train(
-  form = as.formula('outcome ~ a + b + c + d + e + f'),
-  data = model_data,
-  method = 'lm',
-  trControl = caret::trainControl(method = 'cv', number = 5)
+  tictoc::tic(paste0(n_features, ": model fitting"))
+  model_fit <- caret::train(
+    form = glue::glue("outcome ~ {paste0('v', seq_len(n_features), collapse = ' + ')}") |>
+      as.formula(),
+    data = model_data,
+    method = 'lm',
+    trControl = caret::trainControl(method = 'cv', number = 5)
+  )
+  tictoc::toc(log = TRUE)
+
+  tictoc::tic(paste0(n_features, ": shapley decomposition"))
+  marginal_imputer <- mch.ml::MarginalImputer$new(
+    model = model_fit,
+    features_table = model_data,
+    outcomes = model_data$outcome,
+    loss_fun = mch.ml::get_loss_function('L2')
+  )
+  model_sampler <- mch.ml::PermutationSampler$new(imputer = marginal_imputer)
+  model_sampler$sample(run_all = FALSE, min_iterations = 10L)
+  message("Finished decomposition after ", max(model_sampler$importance_tracker$n_obs), " iterations.")
+  tictoc::toc(log = TRUE)
+  tictoc::toc(log = TRUE)
+}
+
+timer_results <- tictoc::tic.log(format = FALSE) |> 
+  lapply(as.data.frame) |> 
+  lapply(data.table::as.data.table) |> 
+  data.table::rbindlist()
+
+timer_subset <- (timer_results
+  [!grepl(':', msg), ]
+  [, .(n_features = as.integer(msg), elapsed = toc - tic) ]
 )
 
-# Initialize models for Shapley value estimation
-marginal_imputer <- mch.ml::MarginalImputer$new(
-  model = model_fit,
-  features_table = model_data,
-  outcomes = model_data$outcome,
-  loss_fun = loss_fun
+# Quick plot
+timer_fig <- ggplot(data = timer_subset, aes(x = n_features, y = elapsed)) +
+  geom_line() + 
+  geom_point(pch = 18) +
+  labs(
+    x = 'Number of features',
+    y = 'Time elapsed (s)',
+    title = "Shapley approximation: fitting time by number of features",
+    subtitle = glue::glue("Dataset with {scales::comma(n_obs)} observations")
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw()
+png(
+  "~/temp_data/usaid-mch-ml/viz/shapley_fitting_timer.png",
+  height = 4, width = 6.5, units = 'in', res = 300
 )
-model_sampler <- mch.ml::PermutationSampler$new(imputer = marginal_imputer, verbose = T)
-
-# Run Shapley decomposition
-model_sampler$sample(run_all = TRUE)
-shapley_vals <- list(
-  absolute = model_sampler$get_shapley_vals(),
-  normalized = model_sampler$get_shapley_vals(normalize = T)
-)
-
-# Print results to console
-message('Absolute Shapley values:')
-knitr::kable(shapley_vals$absolute)
-message("Normalized Shapley values:")
-knitr::kable(shapley_vals$normalized)
+print(timer_fig)
+dev.off()
