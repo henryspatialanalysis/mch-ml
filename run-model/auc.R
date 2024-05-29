65020488e5647cf3267b35ad7644b5cd0ec1f77b
Nathaniel Henry <nat.henry7@gmail.com>
2024-05-29 00:34:49 -0700

    Add out-of-sample metrics.

--- run-model/auc.R
+++ run-model/auc.R
@@ -63,7 +63,8 @@ full_results <- lapply(all_surveys[, .I], function(svy_ii){
         in_holdout <- method_data[, holdout_id == holdout]
         method_data$oos[in_holdout] <- oos$predicted
       }
-      keep_cols <- c(id_cols, 'died', 'is', 'oos')
+      method_data$imputation <- imp
+      keep_cols <- c(id_cols, 'longitude', 'latitude', 'imputation', 'died', 'is', 'oos')
       return(method_data[, ..keep_cols])
     }) |> data.table::rbindlist()
     imputation_results$method <- method
@@ -78,16 +79,108 @@ full_results <- lapply(all_surveys[, .I], function(svy_ii){
 # Combine from age groups to individuals
 combine_q <- function(q_vec) 1 - prod(1 - q_vec)
 indiv_results <- full_results[
-  , c(.(died = max(died)), lapply(.SD, combine_q)),
+  , c(.(died = max(died), x = mean(longitude), y = mean(latitude)), lapply(.SD, combine_q)),
   .SDcols = c('is', 'oos'),
-  by = c('survey_id', 'country', 'method', id_cols)
+  by = c('survey_id', 'country', 'method', 'imputation', id_cols)
 ]
 
 # Get AUC for the individual-level results
 get_auc <- function(obs, pred) as.numeric(pROC::roc(obs, pred, auc = T)$auc) |> suppressMessages()
+get_roc_curve <- function(dt, obs_col = 'died', pred_col = 'oos', max_size = 1000){
+  roc <- pROC::roc(dt[[obs_col]], dt[[pred_col]]) |> suppressMessages()
+  roc_table <- data.table::data.table(
+    tp_rate = rev(roc$sensitivities),
+    fp_rate = rev(1 - roc$specificities)
+  )[order(fp_rate)]
+  # Subsample, if necessary
+  if(max_size < nrow(roc_table)){
+    keep_rows <- c(
+      1:10,
+      seq(11, nrow(roc_table) - 10, length.out = max_size - 20),
+      (-9:0) + nrow(roc_table)
+    )
+    keep_rows <- round(seq(1, nrow(roc_table), length.out = max_size))
+    roc_table <- roc_table[keep_rows, ]
+  } 
+  return(roc_table)
+}
+
+## Calculate and plot AUC by country and method ----------------------------------------->
 
 indiv_auc <- indiv_results[
-  , .(is_auc = get_auc(died, is), oos_auc = get_auc(died, oos)),
-  by = c('survey_id', 'country', 'method')
+  , .(oos_auc = get_auc(died, oos)),
+  by = c('survey_id', 'country', 'method', 'imputation')
 ]
 fwrite(indiv_auc, file = file.path(viz_dir, 'auc_results.csv'))
+
+average_oos_auc <- indiv_auc[, .(oos_auc = mean(oos_auc)), by = c('country', 'method')]
+auc_fill_colors <- RColorBrewer::brewer.pal(name = 'YlGn', n = 9)[2:8]
+auc_plot <- ggplot(data = average_oos_auc, aes(x = method, y = country)) + 
+  geom_tile(aes(fill = oos_auc)) + 
+  geom_text(aes(label = scales::percent(oos_auc, accuracy = 0.1)), color = 'black') + 
+  labs(
+    title = 'Out-of-sample AUC by country and method',
+    x = 'Method',
+    y = 'Country'
+  ) +
+  scale_fill_gradientn(colors = auc_fill_colors, guide = 'none') + 
+  theme_minimal()
+
+png(file.path(viz_dir, 'auc_plot.png'), height = 6, width = 6, units = 'in', res = 300)
+print(auc_plot)
+dev.off()
+
+
+## Plot ROC curves by country ----------------------------------------------------------->
+
+roc_plot_data <- indiv_results[,
+  get_roc_curve(.SD),
+  by = c('survey_id', 'country', 'method', 'imputation')
+]
+roc_plot_data[, imputation_factor := factor(imputation)]
+
+roc_plot <- ggplot() +
+  facet_wrap('country', nrow = 2) + 
+  geom_line(data = roc_plot_data[imputation == 1, ], aes(x = fp_rate, y = tp_rate, color = method), alpha = .5) + 
+  geom_line(data = roc_plot_data[imputation == 2, ], aes(x = fp_rate, y = tp_rate, color = method), alpha = .5) + 
+  geom_line(data = roc_plot_data[imputation == 3, ], aes(x = fp_rate, y = tp_rate, color = method), alpha = .5) + 
+  geom_line(data = roc_plot_data[imputation == 4, ], aes(x = fp_rate, y = tp_rate, color = method), alpha = .5) + 
+  geom_line(data = roc_plot_data[imputation == 5, ], aes(x = fp_rate, y = tp_rate, color = method), alpha = .5) + 
+  geom_abline(intercept = 0, slope = 1, color = '#888888', lwd = 1L, linetype = 2) + 
+  labs(x = 'False positive rate', y = 'True positive rate', color = 'Method') +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(aspect.ratio = 1)
+png(file.path(viz_dir, 'roc_curves.png'), height = 6, width = 12, units = 'in', res = 300)
+print(roc_plot)
+dev.off()
+
+
+## Confusion matrices by country and method --------------------------------------------->
+
+risk_cutoffs <- c(0.1, 0.05)
+
+for(risk_cutoff in risk_cutoffs){
+  message(glue::glue("Risk cutoff at {scales::percent(risk_cutoff)}"))
+  cm <- (indiv_results
+    [, est := (oos >= risk_cutoff)]
+    [, .(
+        tp = round(sum(died & est) / 5),
+        fp = round(sum(!died & est) / 5),
+        fn = round(sum(died & !est) / 5),
+        tn = round(sum(!died & !est) / 5)
+      ), by = .(method, country)
+    ]
+    [, `:=` (
+      precision = scales::percent(tp / (tp + fp), accuracy = 1),
+      recall = scales::percent(tp / (tp + fn), accuracy = 1)
+    )]
+    [order(method, country)]
+  )
+  knitr::kable(cm)
+  fwrite(cm, file = glue::glue("{viz_dir}/confusion_matrix_{risk_cutoff * 100}.csv"))
+}
+
+## Get AUC by subnational unit ---------------------------------------------------------->
+
