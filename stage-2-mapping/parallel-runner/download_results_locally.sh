816a38e4667469cec5f163b96fba6a499b2c8905
Nathaniel Henry <nat.henry7@gmail.com>
2024-08-09 19:09:56 -0700

    Update filepaths.

--- stage-2-mapping/parallel-runner/download_results_locally.sh
+++ stage-2-mapping/parallel-runner/download_results_locally.sh
@@ -12,11 +12,10 @@
 # Connection details
 EC2_USERNAME="ubuntu";
 KEY_PATH="~/.ssh/aws-key-pair-v1.pem";
-EC2_HOST="ec2-35-164-160-53.us-west-2.compute.amazonaws.com";
-MODEL_VERSION="20240805_SEN_wasting_no_ad1";
-SYNC_DIR="~/temp_data/usaid-mch-ml/mbg_model_results/${MODEL_VERSION}"
+EC2_HOST="ec2-54-201-66-112.us-west-2.compute.amazonaws.com";
+RUN_SET="20240805";
+REMOTE_DIR="/home/ubuntu/efs-mount/mbg_outputs/selected_${RUN_SET}"
+LOCAL_DIR="/home/nathenry/temp_data/usaid-mch-ml/mbg_model_results/${RUN_SET}"
 
-mkdir ~/temp_data/usaid-mch-ml/mbg_model_results/$MODEL_VERSION &&
-  rsync -r -e "ssh -i ${KEY_PATH}" \
-    ubuntu@$EC2_HOST:~/efs-mount/mbg_outputs/$MODEL_VERSION/ \
-    ~/temp_data/usaid-mch-ml/mbg_model_results/$MODEL_VERSION/ --info=progress2;
+mkdir $LOCAL_DIR &&
+  rsync -r -e "ssh -i ${KEY_PATH}" "ubuntu@$EC2_HOST:${REMOTE_DIR}" $LOCAL_DIR --info=progress2;
